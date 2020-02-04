50a2fe8ae68794117a49779bdb1d26ebb69de23e
sharadhr <r.sharadh@yahoo.com.sg>
2020-02-05 00:00:07 +0800

    Revert Duke location

---
+++ src/main/java/sharadhr/duke/Duke.java
@@ -0,0 +1,61 @@
+package sharadhr.duke;
+
+import sharadhr.duke.io.Input;
+import sharadhr.duke.io.Output;
+import sharadhr.duke.io.Storage;
+import sharadhr.duke.task.TaskList;
+
+/**
+ * 
+ */
+public class Duke {
+    public static TaskList tasks;
+    public static Input input;
+    public static Output output;
+    public static Storage fileRW;
+
+    public boolean run() {
+        return false;
+    }
+
+    /**
+     * Runs the main program loop.
+     * 
+     * @return {@code false} when the user says 'bye'; otherwise, never returns.
+     */
+    public static boolean programLoop() {
+        boolean exitLoop = false;
+        String command;
+
+        input.nextLine();
+        return false;
+    }
+
+    /**
+     * Cleans up objects and quits the program by calling {@link System#exit(int)}.
+     */
+    public static void exit() {
+        output.sayGoodBye();
+
+        input.close();
+        output.close();
+
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        // Initialises file and UI I/O
+        fileRW = new Storage("data", "duke.txt");
+        input = new Input();
+        output = new Output();
+
+        // Greets the user.
+        output.sayHello();
+
+        // Creates the task list
+        tasks = new TaskList();
+
+        if (programLoop())
+            exit();
+    }
+}
