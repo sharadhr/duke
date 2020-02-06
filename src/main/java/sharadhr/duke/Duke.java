7cd639f03e6c2ab66696cb51f1b6cc948443205c
sharadhr <r.sharadh@yahoo.com.sg>
2020-02-06 20:32:25 +0800

    Properly execute commands

--- src/main/java/sharadhr/duke/Duke.java
+++ src/main/java/sharadhr/duke/Duke.java
@@ -1,61 +1,88 @@
 package sharadhr.duke;
 
+import sharadhr.duke.command.Command;
+import sharadhr.duke.exception.DukeEmptyDetailException;
+import sharadhr.duke.exception.DukeInvalidArgumentException;
+import sharadhr.duke.exception.DukeInvalidCommandException;
+import sharadhr.duke.exception.DukeInvalidDateException;
 import sharadhr.duke.io.Input;
 import sharadhr.duke.io.Output;
 import sharadhr.duke.io.Storage;
 import sharadhr.duke.task.TaskList;
 
+import java.util.Optional;
+
 /**
- * 
+ *
  */
-public class Duke {
-    public static TaskList tasks;
-    public static Input input;
-    public static Output output;
-    public static Storage fileRW;
+public class Duke
+{
+	public static TaskList tasks;
+	public static Input input;
+	public static Output output;
+	public static Storage fileRW;
 
-    public boolean run() {
-        return false;
-    }
+	/**
+	 * Runs the main program loop.
+	 *
+	 * @return {@code false} when the user says 'bye'; otherwise, never returns.
+	 */
+	public static boolean programLoop()
+	{
+		boolean isExit = false;
+		while (!isExit)
+		{
+			try
+			{
+				Optional<Command> possibleCommand = input.nextLine().getCommand();
+				if (!possibleCommand.isPresent())
+				{
+					output.say("Input cannot be empty; please enter a command.");
+					continue;
+				}
+				possibleCommand.get().execute(tasks, fileRW, output);
 
-    /**
-     * Runs the main program loop.
-     * 
-     * @return {@code false} when the user says 'bye'; otherwise, never returns.
-     */
-    public static boolean programLoop() {
-        boolean exitLoop = false;
-        String command;
+			}
+			catch (DukeInvalidArgumentException | DukeInvalidCommandException | DukeEmptyDetailException | DukeInvalidDateException e)
+			{
+				output.sayError(e);
+			}
+		}
+		return false;
+	}
 
-        input.nextLine();
-        return false;
-    }
+	/**
+	 * Cleans up objects and quits the program by calling {@link System#exit(int)}.
+	 */
+	public static void exit()
+	{
+		output.sayGoodBye();
 
-    /**
-     * Cleans up objects and quits the program by calling {@link System#exit(int)}.
-     */
-    public static void exit() {
-        output.sayGoodBye();
+		input.close();
+		output.close();
 
-        input.close();
-        output.close();
+		System.exit(0);
+	}
 
-        System.exit(0);
-    }
+	public static void main(String[] args)
+	{
+		// Initialises file and UI I/O
+		fileRW = new Storage("data", "duke.txt");
+		input = new Input();
+		output = new Output();
 
-    public static void main(String[] args) {
-        // Initialises file and UI I/O
-        fileRW = new Storage("data", "duke.txt");
-        input = new Input();
-        output = new Output();
+		// Greets the user.
+		output.sayHello();
 
-        // Greets the user.
-        output.sayHello();
+		// Creates the task list
+		tasks = fileRW.loadFromFile();
 
-        // Creates the task list
-        tasks = new TaskList();
+		if (!programLoop())
+			exit();
+	}
 
-        if (programLoop())
-            exit();
-    }
+	public boolean run()
+	{
+		return false;
+	}
 }
