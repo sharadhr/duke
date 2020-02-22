51d99c1611761efc3b5b10b02313e127efe1bfc7
sharadhr <r.sharadh@yahoo.com.sg>
2020-02-22 23:14:28 +0800

    Fix done command

--- src/main/java/sharadhr/duke/command/DoneCommand.java
+++ src/main/java/sharadhr/duke/command/DoneCommand.java
@@ -12,26 +12,22 @@ import sharadhr.duke.task.TaskList;
 public class DoneCommand extends Command {
     private int position;
 
-    DoneCommand(String[] commandTokens, int position) throws DukeInvalidArgumentException {
-
-        // if (this.argumentTokens.length != 1 || !this.argumentTokens[0].matches("\\d+")) {
-        //     throw new DukeInvalidArgumentException(argument, this.commandName, this.getClass()
-        //     .getSimpleName());
-        // }
-        // this.commandName = CommandName.DONE;
-        // this.position = position;
-    }
-
     /**
      * @param argumentTokens
      * @throws DukeInvalidArgumentException
      */
     public DoneCommand(String[] argumentTokens) throws DukeInvalidArgumentException {
-        this(argumentTokens, Integer.parseInt(argumentTokens[1]));
+        super(argumentTokens);
+        if (argumentTokens.length != 1 || !argumentTokens[0].matches("\\d+"))
+        {
+            throw new DukeInvalidArgumentException("Done command should have only one positive number as argument.",
+                    argumentTokens, commandName, this.getClass().getSimpleName());
+        }
+        this.position = Integer.parseInt(this.argumentTokens[0]);
     }
 
     @Override public void execute(TaskList tasks, Storage storage, Output output) {
-        if (!(this.position >= 1) && this.position <= tasks.numberOfTasks()) {
+        if ((this.position >= 1) && this.position <= tasks.numberOfTasks()) {
             Task toComplete = tasks.getTaskAtPosition(position);
             if (toComplete.markComplete()) {
                 output.sayTaskMarkedComplete(toComplete);
